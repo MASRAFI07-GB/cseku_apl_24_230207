cc0c0284360b248d62fd1f53208743f2caab306e
MASRAFI MURTOJA <230207@ku.ac.bd>
2024-09-24 11:10:52 +0600

    a_string_is_palindrome_or_not_new.cpp

--- task1/a_string_is_palindrome_or_not_new.cpp
+++ task1/a_string_is_palindrome_or_not_new.cpp
@@ -1,4 +1,4 @@
-
+// taking some string and cheaks whether they are palindrome or not
 #include <bits/stdc++.h>
 #include <string.h>
 using namespace std;
@@ -8,11 +8,13 @@ int isPalidrome(char s1[])
     int length = strlen(s1);
     char s2[100];
     int z = 0;
+    // insert the chracter of s1 to s2 in reverse order
     for (int i = length - 1; i >= 0; i--)
     {
         s2[z++] = s1[i];
     }
     s2[z] = '\0';
+    // comparing s1 and s2
     int flag = strcmp(s1, s2);
     if (flag == 0)
         return 1;
@@ -22,6 +24,7 @@ int isPalidrome(char s1[])
 int main()
 {
     int numofString;
+    cout << "enter the number of string:";
     cin >> numofString;
     char name[numofString][100];
     for (int i = 0; i < numofString; i++)
